29f2f3d96ff0d5c3581d05be8dda40325f02fb36
Eduardo-921 <eduardo.maito@escola.pr.gov.br>
2026-08-28 12:26:48 -0300

    2

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector("button");
+    botao.addEventListener("click", botaoclicado);
+    function botaoclicado(){
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+        texto.textContent++;
+}
